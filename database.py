631961ecd78c90a9afe5624bdee4c90f1ea7b0f5
Rhennan <reehsecundario@gmail.com>
2026-04-29 22:33:21 -0300

    teste2

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+# Versão com SQLite 
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
@@ -15,3 +17,35 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+
+# Versão Original 
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, DeclarativeBase
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# class Base(DeclarativeBase):
+#     pass
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
